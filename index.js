b952193f73e87aa3a9da8a6618391f524e9a0ab8
SMI <91237098+sandeep-manivannan@users.noreply.github.com>
2022-04-03 19:04:20 -0700

    c

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ window.onscroll = function() {scroll()};
 function scroll() {
     if ( document.documentElement.scrollTop > 480) {
         document.getElementById("navigation").style.cssText = "background:whitesmoke; opacity:0.7; "
-        document.getElementById("content").style.cssText =""
     }
      else {
         document.getElementById("navigation").style.cssText =""
-        document.getElementById("content").style.cssText = "position: relative;top: 11vh;left: -100vh; transition: 2s;"
     }
 };
 function navbarOpen() {
