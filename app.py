64cad91547f0126b520b6da143f7f9288f9217a0
SMI <91237098+sandeep-manivannan@users.noreply.github.com>
2022-04-09 17:00:08 -0700

    a

--- app.py
+++ app.py
@@ -1,18 +1,73 @@
-from flask import Flask, render_template,redirect,url_for
+from flask import Flask, render_template,redirect,url_for,request,flash
+import os
+import sqlite3
 
 app = Flask(__name__)
 
+app.secret_key = "sandeep"
+con=sqlite3.connect("image.db")
+con.execute("create table if not exists image(pid integer primary key,img TEXT)")
+con.close()
+
+app.config['UPLOAD_FOLDER']="static\img"
+users = {'sandeep':'sandeep0','sandz':'sandz0'}
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title="Vnex")
 
 @app.route("/admin")
 def add():
-    return render_template('admin.html')
+    return render_template('admin.html', title="admin")
 
 @app.route("/login")
 def log():
-    return render_template('login.html')
+    return render_template('login.html', title="login")
+
+# @app.route('/admin_page',methods=['POST' ,'GET'])
+# def admin():
+#    name = request.form['username']
+#   pass = [request.form['password']
+#    if name not in users:
+#        return render_template('login.html')
+#        flash('Invalid username or password')
+#    else:
+#        if users[name]!= pass:
+#            return render_template('login.html')
+#            flash('invalid username or password')
+#        else:
+#            return render_template('admin.html')
+@app.route('/upload', methods=['GET', 'POST'])
+def uploadimg():
+
+    con = sqlite3.connect("image.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("select * from image")
+    data = cur.fetchall()
+    con.close()
+
+    if request.method == 'POST':
+        upload_image = request.files['upload_image']
+         
+        if upload_image.filename != '':
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], upload_image.filename)
+            upload_image.save(filepath)
+            con = sqlite3.connect("image.db")
+            cur = con.cursor()
+            cur.execute("insert into image(img)values(?)", (upload_image.filename, ))
+            con.commit()
+            flash("file uploaded successfully")
+
+            con = sqlite3.connect("image.db")
+            con.row_factory = sqlite3.Row
+            cur = con.cursor()
+            cur.execute("select * from image")
+            data = cur.fetchall()
+            con.close()
+            return render_template("index.html", data=data)
+            
+    return render_template("index.html", data=data)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- static/login.js
+++ static/login.js
@@ -1,27 +1,17 @@
-function navbarOpen() {
-    document.getElementById("navbarcont").style.left = "0vh"
-};
-function navbarClose() {
-   document.getElementById("navbarcont").style.left = "100vh"
-};
-
-function data() {
+function daa() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var error = document.getElementById("error")
 
-if (username == "sandeep" && password == "sandeep") {
+    if (username == "sandeep" && password == "sandeep") {
     alert("done");
-    location.href = '/admin'
-} 
-else if (username == "admin" && password == "san") {
+    location.href = 'login'
+    } else if (username == "admin" && password == "san") {
     alert("done");
-    location.href = '/admin'
-} 
-else {
+    window.location.href = 'admin'
+    } else {
     error.style.cssText = "opacity: 0.9;";
     alert("invalid username or password");
     location.reload();
     }
-    return 0;
 };
