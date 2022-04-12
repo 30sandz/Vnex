693f27220505cc07b515a4dc61cdb57695e9ed9b
SMI <91237098+sandeep-manivannan@users.noreply.github.com>
2022-04-12 13:19:19 -0700

    sa

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from turtle import title
 from webbrowser import get
 from flask import Flask, render_template,redirect,url_for,request,flash
 import os
@@ -21,11 +22,21 @@ def home():
 def add():
     return render_template('admin.html', title="admin")
 
+@app.route("/image")
+def img():
+    con = sqlite3.connect("image.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("select * from image")
+    data = cur.fetchall()
+    con.close()
+    return render_template('images.html', data=data, title="Data")
+
 @app.route("/login")
 def log():
     return render_template('login.html', title="login")
 
-@app.route('/admin_page',methods=['POST' ,'GET'])
+@app.route('/login_page',methods=['POST' ,'GET'])
 def admin():
     if request.method == 'POST':
         name = request.form["username"]
@@ -33,26 +44,25 @@ def admin():
 
         if name not in users:
             return render_template('login.html')
-            flash('Invalid username or password')
+            flash('Invalid Username or Password' ,'error')
         else:
             if users[name]!= password:
                 return render_template('login.html')
-                flash('invalid username or password')
+                flash('Invalid Username or Password', 'error')
             else:
                 return render_template('admin.html')
+                flash('Login successful')
 @app.route('/upload', methods=['GET', 'POST'])
 def uploadimg():
-
     con = sqlite3.connect("image.db")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
     cur.execute("select * from image")
     data = cur.fetchall()
     con.close()
-
     if request.method == 'POST':
         upload_image = request.files['upload_image']
-         
+
         if upload_image.filename != '':
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], upload_image.filename)
             upload_image.save(filepath)
@@ -68,9 +78,9 @@ def uploadimg():
             cur.execute("select * from image")
             data = cur.fetchall()
             con.close()
-            return render_template("index.html", data=data)
+            return render_template("index.html")
             
-    return render_template("index.html", data=data)
+    return render_template("admin.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
